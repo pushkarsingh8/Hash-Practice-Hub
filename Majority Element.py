cc49586333f65b099803521c9f042d819c38e6e0
Pushkar Singh <2pushkarsingh08@gmail.com>
2025-03-23 19:33:09 +0530

    Some updates

---
+++ Majority Element.py
@@ -0,0 +1,43 @@
+#Majority Element:-
+
+def Find_Majority_Element(arr):
+
+    n = len(arr)
+
+    dict = {}
+
+    for x in arr:
+
+        if x not in dict:
+
+            dict[x] = 1
+
+
+        else:
+
+
+            dict[x] += 1
+
+
+    
+    for num,value in dict.items():
+
+        if value > n//2:
+
+            print("Majority Element: ",num)
+            break
+
+
+    print(-1)
+
+
+
+arr = [3, 3, 4, 2, 4, 4, 2, 4]
+
+Find_Majority_Element(arr)
+
+
+
+
+
+
